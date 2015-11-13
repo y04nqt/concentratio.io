7517620d24f80c65af19f58f823eef60293f5f6c
Aaron <akr.git.receiver@gmail.com>
2015-11-13 08:29:49 -0600

    added concentration game + moved directories elsewhere

--- scripts/app.js
+++ scripts/app.js
@@ -1,188 +1,159 @@
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
-//beginning of the Start Menu
-function StartMenu(){
-  this.title = "Chowed";
-  this.mode = "easy",
-  this.numOfPlayers = 1;
-  this.background;
-  this.start = false;
-  // this.onStart = function() {
-  //   // css in here
-  //   $('body').css('background-color', 'red');
-  //   this.start = false;
-  // }
-  this.toggleMenu = function() {
-    if (this.start == false) {
-      $('body').append('<div class="start-menu">').css('background-color', '#eff');
-      $('.start-menu').append('<button id="hello_btn">');
-      $('#hello_btn').html('hi');
-      // update this.start
-      this.start = true;
-    } else {
-      $('#hello_btn').css('background-color', '#cdd');
-      $('.start-menu').remove();
+//constructing the Gameboard object
+function Gameboard(){
+  //adding in properties because bosses do that
+  this.title = "HexMatch";
+  this.tileArray = ['a','a', 'b','b', 'c','c', 'd','d', 'e','e', 'f','f'];
+  this.tileCount = this.tileArray.length;
+  this.memoryTiles = [];
+  this.memoryTilesIds =[];
+  this.tileClicked = false;
+  this.tilesOut = [];
+  this.tilesFlipped = 0;
+  //end of cool ass properties
+
+  //fisher-yates shuffle algorithm
+  this.tileArrayShuffle = function(){
+    var currentIndex = this.tileArray.length, tempVal, randIndex;
+
+    while( 0 < currentIndex){
+      //create random index int
+      randIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -=1;
+
+      tempVal = this.tileArray[currentIndex];
+      this.tileArray[currentIndex] = this.tileArray[randIndex];
+      this.tileArray[randIndex] = tempVal;
     }
+    return this.tileArray;
+  }
+  //end shuffle
+  //generate the tiles
+  this.generateTiles = function(){
+    gameBoard.tileArrayShuffle();
+    for(i=0; i < gameBoard.tileArray.length; i++){
+      $('#theBoard').append('<div id="tile_'+i+'" class="tiles" onclick="gameBoard.flipTiles(this,\''+gameBoard.tileArray[i]+'\')">'+gameBoard.tileArray[i]+'</div>');
+    }
+    $('body').append('<h1>');
+  }
+  //end generation
+  this.flipTiles = function(tile, val){
+
+      if($(tile).html = ' ' && gameBoard.memoryTiles.length < 2){
+        //setting tile css with style objects
+        var tileCSS ={
+          'color' : 'white',
+          'backgroundColor': 'rgba(0, 0, 0, 0.21)',
+
+        }
+        $(tile).css(tileCSS);
+        //ending css change
+        //if statement to grab the first value
+        if(gameBoard.memoryTiles.length == 0){
+          //this pushes the clicked value into two arrays
+          gameBoard.memoryTiles.push(val);
+          gameBoard.memoryTilesIds.push(tile.id);
+          //ends the pushing
+          //logging the push
+          console.log(gameBoard.memoryTiles);
+          console.log(gameBoard.memoryTilesIds);
+          //end of logging
+        }else if(gameBoard.memoryTiles.length == 1){
+          gameBoard.memoryTiles.push(val);
+          gameBoard.memoryTilesIds.push(tile.id);
+          console.log(gameBoard.memoryTiles);
+          console.log(gameBoard.memoryTilesIds);
+          if(gameBoard.memoryTiles[0] == gameBoard.memoryTiles[1]){
+            gameBoard.tilesFlipped += 2;
+            gameBoard.tileClicked == true;
+            gameBoard.tilesOut += gameBoard.memoryTiles;
+            console.log(gameBoard.tilesFlipped);
+            var secretScoreHolder = gameBoard.tilesFlipped;
+            $('h1').html(secretScoreHolder);
+
+            //gameBoard.memoryTiles = [];
+            gameBoard.memoryTilesIds =[];
+            var TileOne = gameBoard.memoryTiles[0];
+            var TileTwo = gameBoard.memoryTiles[1];
+
+
+            $(gameBoard.memoryTiles[0]).remove();
+            $(gameBoard.memoryTiles[1]).remove();
+            // if()
+            if(gameBoard.tilesFlipped == gameBoard.tileArray.length){
+              $('#theBoard').html('');
+              alert('cleared board')
+              var cnfm = confirm('Want to play again?');
+              cnfm;
+              if(cnfm === true){
+                gameBoard.tilesFlipped = 0;
+                $('h1').remove();
+                gameBoard.generateTiles();
+              }else if(cnfm === false){
+                window.close();
+              }else{
+                alert('error');
+              }
+            }
+          }else{
+            var firstTile = gameBoard.memoryTilesIds[0];
+            var secondTile = gameBoard.memoryTilesIds[1];
+
+          }
+          setTimeout(gameBoard.flipBack, 700);
+        }
+
+      }
+      console.log(tile+' '+val);
+  }
+  this.flipBack = function(){
+    var defaultStyle = {
+      'color' : 'transparent',
+      'backgroundColor': 'rgba(0, 0, 0, 0.71)',
+      'opacity' : '1'
+    }
+    var firstTile = gameBoard.memoryTilesIds[0];
+    var secondTile = gameBoard.memoryTilesIds[1];
+    firstTile = '#'+firstTile;
+    secondTile = '#'+secondTile;
+    $(firstTile).css(defaultStyle);
+    $(secondTile).css(defaultStyle);
+    console.log('flipBack');
+    console.log(firstTile);
+    console.log(secondTile);
+    gameBoard.memoryTiles = [];
+    gameBoard.memoryTilesIds =[];
   }
 }
-//end of Start Menu
-////////////////////////////////
-//getAtkPwr function start
-var getAtkPwr = function(min, max){
-  return Math.floor(Math.random() *(max - min))+min;
-}
-//end of above
-////////////////////////////////
-//created Player constructor
-function Player(){
-    this.hp = 0;
-    this.armourRating = 0;
-    this.mana = 50;
-    this.minAtk = 10;
-    this.maxAtk = 50;
-    this.atkPower = 0;
-    this.weapon = 0;
-    this.armour = 0;
-    this.weaponPieces = [];
-    this.armourPieces = [];
-    this.noodles = 0;
-    this.fruitJuice = 2;
-}
-//end of constructor
-////////////////////////////////
-//PlayerOne constructed
-var PlayerOne = new Player();
-PlayerOne.hp = 100;
-PlayerOne.minAtk = 200;
-PlayerOne.maxAtk = 500;
-PlayerOne.atkPower = getAtkPwr(PlayerOne.minAtk, PlayerOne.maxAtk);
-//end of construction
-////////////////////////////////
-//PlayerTwo constructed
-var PlayerTwo = new Player();
-PlayerTwo.hp = 101;
-PlayerTwo.minAtk = 200;
-PlayerTwo.maxAtk = 500;
-PlayerTwo.atkPower = getAtkPwr(PlayerTwo.minAtk, PlayerTwo.maxAtk);
-//end of construction
-////////////////////////////////
-//get a number between zero and one hundred
-var getZeroToHundred = function(){
-  return Math.floor(Math.random()*100);
-}
-//end of function
-////////////////////////////////
-//beginning of Creature constructor
-function Creature(){
-  this.hp = 25;
-  this.atkPower = 0;
-  this.minAtk = 2;
-  this.minAtk = 10;
-  this.loot = ['common', 'rare'];
-  this.lootDropCommon = ['Simple Spade', 'Pointy Barb', 'Sharp Sword', 'Mauling Mace' ];
-  this.lootDropRare = 'Booming Boomstick';
-  this.foodDrop = 1;
-  this.manaDrop = getZeroToHundred();
-  this.name = ['Terrifying Turtle', 'Gigantic Giraffe', 'Fiery Fox', 'Slithery Snake', 'Immutable Malamute', 'Spooky Skeleton', 'Spinny Spider'];
-
-}
-var creature = new Creature();
-//end of Creature constructor
-////////////////////////////////
-//beginning of Boss constructor
-function Boss(){
-  this.hp = 125;
-  this.atkPower = 10;
-  this.minAtk = 3;
-  this.maxAtk = 15;
-  this.loot = 'Stupendous';
-  this.lootDropArmour = ['Serendipitous Shroud of Splendor', 'Malformed Runic Tunic', 'Bastardized Boots of Bewilderment', 'Guilded Gloves of the Grave', 'Serrated Edge Spaulders', 'Leonidas Warring Leggaurds'];
-  this.name = ['Guggenheim Gargatron', 'RizzRocket Sprocket Mechanair', 'Menacing Mastigoproctus', 'Grimey Goblin Gurkon', 'Heretic Hellwolf', 'The Droid I was looking for'];
-  this.bossLives = 5;
-}
-var boss = new Boss();
-//end of Boss construsctor
-////////////////////////////////
-//beginning of Level constructor
-function Dungeon(){
-  this.lairs = ['Circle of Lust', 'Circle of Gluttons', 'Circle of Heretics', 'Circle of Corruption', 'The Pit'];
-  this.name = 'The Sacred Tomb';
-  this.background;
-  this.getLairs = function(lair){
-    switch (lair) {
-      case 1:
-        return this.lairs[0];
-        break;
-      // case 2:
-      //   return this.lairs[1];
-      // case 3:
-      //   return this.lairs[2];
-      // case 4:
-      //   return this.lairs[3];
-      // case 5:
-      //   return this.lairs[4];
-      default:
-        return 'shit';
-        break;
-    };
-  }
-  this.setLairTitle = function(lair){
-    switch (lair) {
-      case 1:
-        var title = $('<h1>');
-        $('.first-level').append(title);
-        $(title).html(this.lairs[0]);
-        $('.first-level').append('<button id="creatureBtn">').append('<button id="eatBtn">').append('<button id="bossBtn">').append('<button id="drinkBtn">');
-        $('#creatureBtn').click(function(){
-          $('.first-level').append('<p id="bing">');
-          $('#bing').html('bing');
-        });
-        break;
-      // case 2:
-      //   return this.lairs[1];
-      // case 3:
-      //   return this.lairs[2];
-      // case 4:
-      //   return this.lairs[3];
-      // case 5:
-      //   return this.lairs[4];
-      default:
-        return 'shit';
-        break;
-    };
-  }
-}
-var theGrave = new Dungeon();
-//end of Dungeon Constructor
-////////////////////////////////
-
-// console.log(PlayerOne.hp);
-// console.log(PlayerOne.atkPower);
-// console.log(PlayerTwo.hp);
-// console.log(PlayerTwo.atkPower);
-// console.log(creature.manaDrop);
-
-
-
-//theGrave.background = theGrave.lairs[0]; save for later
-
-
-
+var gameBoard = new Gameboard();
 
 
 $(document).ready(function(){
-  startMenu = new StartMenu();
-  startMenu.toggleMenu();
-  $('#hello_btn').click(function(){
-    startMenu.toggleMenu();
-    $('body').append('<div class="first-level">');
-    //$('.first-level').html(theGrave.getLairs(1));
-    //startMenu.start = true;
-    //console.log(startMenu);
-    theGrave.setLairTitle(1);
-  });
-});
+  $('body').append('<div id="theBoard">');
+  gameBoard.generateTiles();
+  console.log(gameBoard.tileCount);
+})
+
+
+// var Card = function() {
+//
+// 	this.tileDiv = $('div').prop('id');
+// 	this.isFlipped = false;
+//
+// 	var base = this;
+// 	$(this.el).on('click', function(tile, val) {
+// 		base.toggle();
+// 	});
+//
+// 	this.toggle = function() {
+// 		$(base.el).css()
+// 	}
+//
+// 	this.initialize = function() {
+// 		$('#theBoard').append(base.tileDiv);
+// 	}
+//   this.render = function(){
+//
+//   }
+// 	this.initialize();
+//
+// }
